9c1ba12691723397edf2502253fd7fee2a65af23
nblaye <nblaye@smartadserver.com>
2022-12-16 16:56:33 +0100

    Day 16 Part 1

---
+++ 12-16/main.py
@@ -0,0 +1,227 @@
+import re
+
+
+class Valve:
+    opened = False
+
+    def __init__(self, name, flow_rate, children):
+        self.name = name
+        self.flow_rate = flow_rate
+        self.children = children
+
+    def is_useful(self):
+        return self.flow_rate > 0
+
+    def __str__(self):
+        return f"{self.name} : {self.flow_rate} : {self.children}"
+
+
+def distance_from_start_to_all_valve_rec(children, valves, distance, visited, current_distance):
+    next_children = []
+    for child in children:
+        if child not in visited:
+            visited.add(child)
+            distance[child] = current_distance
+            valve = valves[child]
+            next_children.extend(valve.children)
+    if next_children:
+        return distance_from_start_to_all_valve_rec(next_children, valves, distance, visited, current_distance + 1)
+    else:
+        return distance
+
+
+def distance_from_start_to_all_valve(start_valve, valves):
+    current_distance = 0
+    distance = {start_valve.name: 0}
+    visited = set()
+    visited.add(start_valve.name)
+    return distance_from_start_to_all_valve_rec(start_valve.children, valves, distance, visited, current_distance + 1)
+
+
+def compute_distance(useful_valves, valves):
+    distances = {}
+    for valve in useful_valves:
+        distance = distance_from_start_to_all_valve(valve, valves)
+        distances[valve.name] = distance
+    return distances
+
+
+def parse_input_line(line):
+    p = re.compile(r'Valve (\w+) has flow rate=(\d+); tunne(l|ls) lea(ds|d) to valv(es|e) (.*)')
+    matches = p.match(line)
+    valve = matches.group(1)
+    flow_rate = int(matches.group(2))
+    children = matches.group(6).strip().split(", ")
+    return Valve(valve, flow_rate, children)
+
+
+def compute_potential_flow_rate(distance, valves, minutes_left):
+    potential_flow_rate_by_valve = {}
+    for valve_name, d in distance.items():
+        valve = valves[valve_name]
+        flow_rate = valve.flow_rate * (minutes_left - d - 1)
+        potential_flow_rate_by_valve[valve_name] = flow_rate
+    return potential_flow_rate_by_valve
+
+
+def release_pressure_rec(valve, valves, minutes_left, opened, visited):
+    if minutes_left < 2 or valve.name in visited:
+        return 0
+    # print(minutes_left)
+    # print(valve)
+    new_visited = visited.copy()
+    new_visited.add(valve.name)
+    flow_rates = []
+    ### We do not open it
+    for child in valve.children:
+        valve_child = valves[child]
+        flow_rate = release_pressure_rec(valve_child, valves, minutes_left - 1, opened.copy(), new_visited)
+        flow_rates.append(flow_rate)
+
+    if valve.flow_rate > 0 and valve.name not in opened:
+        new_opened = opened.copy()
+        new_opened.add(valve.name)
+        flow_rate = (minutes_left - 1) * valve.flow_rate
+        for child in valve.children:
+            valve_child = valves[child]
+            child_flow_rate = release_pressure_rec(valve_child, valves, minutes_left - 2, new_opened, set()) + flow_rate
+            flow_rates.append(child_flow_rate)
+
+    return max(flow_rates)
+
+
+def release_pressure_useful_rec(valve, valves, minutes_left, opened, distances, useful_valves):
+    if minutes_left < 2:
+        return 0
+    flow_rates = []
+
+    new_opened = opened.copy()
+    if valve.flow_rate > 0:
+        new_opened.add(valve.name)
+        minutes_left -= 1
+        flow_rate = minutes_left * valve.flow_rate
+    else:
+        flow_rate = 0
+
+    for useful_valve in useful_valves:
+        if useful_valve not in new_opened:
+            distance = distances[valve.name][useful_valve]
+            child_flow_rate = release_pressure_useful_rec(valves[useful_valve], valves, minutes_left - distance,
+                                                          new_opened, distances, useful_valves)
+            flow_rates.append(child_flow_rate)
+
+    if flow_rates:
+        return max(flow_rates) + flow_rate
+    else:
+        return 0
+
+
+def release_pressure_useful_dual_rec(valve_h, valve_e, valves, minutes_left_h, minutes_left_e, opened, distances,
+                                     useful_valves):
+    if minutes_left_h < 2 and minutes_left_e < 2:
+        return 0
+    flow_rates = []
+
+    flow_rate = 0
+
+    ## human player
+    if valve_h.flow_rate > 0 and valve_h.name not in opened:
+        opened.add(valve_h.name)
+        minutes_left_h -= 1
+        flow_rate = minutes_left_h * valve_h.flow_rate
+
+    ## elephant player
+    elif valve_e.flow_rate > 0 and valve_e.name not in opened:
+        opened.add(valve_e.name)
+        minutes_left_e -= 1
+        flow_rate = minutes_left_e * valve_e.flow_rate
+
+    for useful_valve in useful_valves:
+        if useful_valve not in opened:
+            # move human first
+            if minutes_left_h >= minutes_left_e:
+                # human
+                distance_h = distances[valve_h.name][useful_valve]
+                child_flow_rate = release_pressure_useful_dual_rec(valves[useful_valve], valve_e, valves,
+                                                                   minutes_left_h - distance_h, minutes_left_e, opened.copy(),
+                                                                   distances, useful_valves)
+                flow_rates.append(child_flow_rate)
+            else:
+                # elephant
+                distance_e = distances[valve_e.name][useful_valve]
+                child_flow_rate = release_pressure_useful_dual_rec(valve_h, valves[useful_valve], valves,
+                                                                   minutes_left_h, minutes_left_e - distance_e, opened.copy(),
+                                                                   distances, useful_valves)
+                flow_rates.append(child_flow_rate)
+
+    if flow_rates:
+        return max(flow_rates) + flow_rate
+    else:
+        return 0
+
+
+def release_pressure(valve, valves, minutes_left):
+    return release_pressure_rec(valve, valves, minutes_left, set(), set())
+
+
+def release_pressure_useful(valve, valves, minutes_left, distances, useful_valves):
+    return release_pressure_useful_rec(valve, valves, minutes_left, set(), distances, useful_valves)
+
+
+def release_pressure_useful_dual(valve, valves, minutes_left, distances, useful_valves):
+    return release_pressure_useful_dual_rec(valve, valve, valves, minutes_left, minutes_left, set(), distances,
+                                            useful_valves)
+
+
+def main():
+    with open("input.txt") as f:
+        lines = f.readlines()
+
+    valves = dict()
+    for line in lines:
+        valve = parse_input_line(line)
+        valves[valve.name] = valve
+
+    return release_pressure(valves['AA'], valves, 30)
+
+
+# Optimize exercise 1
+def main_bis():
+    with open("input.txt") as f:
+        lines = f.readlines()
+
+    valves = dict()
+    valve_list = []
+    useful_valves = []
+    for line in lines:
+        valve = parse_input_line(line)
+        valves[valve.name] = valve
+        valve_list.append(valve)
+        if valve.is_useful():
+            useful_valves.append(valve.name)
+
+    distances = compute_distance(valve_list, valves)
+    print(distances)
+    return release_pressure_useful(valves['AA'], valves, 30, distances, useful_valves)
+
+
+def main2():
+    with open("input.txt") as f:
+        lines = f.readlines()
+
+    valves = dict()
+    valve_list = []
+    useful_valves = []
+    for line in lines:
+        valve = parse_input_line(line)
+        valves[valve.name] = valve
+        valve_list.append(valve)
+        if valve.is_useful():
+            useful_valves.append(valve.name)
+
+    distances = compute_distance(valve_list, valves)
+    return release_pressure_useful_dual(valves['AA'], valves, 26, distances, useful_valves)
+
+
+if __name__ == '__main__':
+    print(main2())
