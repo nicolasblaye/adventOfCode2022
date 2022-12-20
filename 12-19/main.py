7ea99799549a6dc825b1f32335accc3b1ad408f3
nblaye <nblaye@smartadserver.com>
2022-12-20 12:03:04 +0100

    Day 19, part1 ok

--- 12-19/main.py
+++ 12-19/main.py
@@ -1,4 +1,6 @@
-from model import Blueprint, OreRobot, ClayRobot, ObsidianRobot, GeodeRobot
+from copy import deepcopy
+
+from model import Blueprint, OreRobot, ClayRobot, ObsidianRobot, GeodeRobot, Solution, Geode, Ore, Clay, Obsidian
 import re
 
 pattern = re.compile("Blueprint (\d+): Each ore robot costs (\d+) ore."
@@ -6,7 +8,6 @@ pattern = re.compile("Blueprint (\d+): Each ore robot costs (\d+) ore."
                      " Each obsidian robot costs (\d+) ore and (\d+) clay."
                      " Each geode robot costs (\d+) ore and (\d+) obsidian."
                      )
-
 
 def parse_input(path):
     blueprints = []
@@ -23,12 +24,76 @@ def parse_input(path):
     return blueprints
 
 
+def compute_n_turns(turns_left, solution):
+    new_solution = Solution(solution.blueprint, ores=deepcopy(solution.ores), robots=deepcopy(solution.robots),
+                            turn_played=solution.turn_played, created_at=deepcopy(solution.created_at))
+    if turns_left == 1:
+        new_solution.play_turn(1)
+        return new_solution
+    child_solutions = []
+    time_for_next_robot = new_solution.time_for_next_robot()
+    # if we can build geode, do it
+    if time_for_next_robot[new_solution.blueprint.geode_robot] == 0:
+        robot, time = new_solution.blueprint.geode_robot, 0
+        if time + 1 <= turns_left:
+            new_new_solution = Solution(new_solution.blueprint, ores=deepcopy(new_solution.ores),
+                                        robots=deepcopy(new_solution.robots), turn_played=new_solution.turn_played, created_at=deepcopy(new_solution.created_at))
+            new_new_solution.play_turn(time + 1)
+            new_new_solution.create(robot, turns_left - time - 1)
+            child_solutions.append(compute_n_turns(turns_left-time-1, new_new_solution))
+        else:
+            child_solutions.append(new_solution)
+    else:
+        for robot, time in time_for_next_robot.items():
+            if time + 1 <= turns_left:
+                new_new_solution = Solution(new_solution.blueprint, ores=deepcopy(new_solution.ores),
+                                            robots=deepcopy(new_solution.robots), turn_played=new_solution.turn_played, created_at=deepcopy(new_solution.created_at))
+                new_new_solution.play_turn(time + 1)
+                new_new_solution.create(robot, turns_left - time - 1)
+                child_solutions.append(compute_n_turns(turns_left-time-1, new_new_solution))
+            else:
+                child_solutions.append(new_solution)
+    if child_solutions:
+        return max(child_solutions, key=lambda x: int(x.ores[Geode()]))
+    else:
+        return solution
+
+
 def main(input_path):
     blueprints = parse_input(input_path)
 
+    max_geode = []
     for blueprint in blueprints:
-        print(blueprint)
+        solution = Solution(blueprint, ores = {Ore() : 0, Clay() : 0, Obsidian() : 0, Geode() : 0}, robots = {}, turn_played = 0, created_at = [])
+        solution.play_turn(1)
+        best_solution = compute_n_turns(23, solution)
+        if Geode() in best_solution.ores:
+            max_geode.append(best_solution.ores[Geode()] * int(blueprint.b_id))
+            #print(best_solution)
+            print(f"max for blueprint {blueprint.b_id} is {best_solution.ores[Geode()]}")
+    return max_geode
+
+
+def main2(input_path):
+    blueprints = parse_input(input_path)
+    blueprints = blueprints[:3]
+    print(len(blueprints))
+
+    max_geode = []
+    for blueprint in blueprints:
+        solution = Solution(blueprint, ores = {Ore() : 0, Clay() : 0, Obsidian() : 0, Geode() : 0}, robots = {}, turn_played = 0, created_at = [])
+        solution.play_turn(1)
+        best_solution = compute_n_turns(32, solution)
+        if Geode() in best_solution.ores:
+            max_geode.append(best_solution.ores[Geode()])
+            #print(best_solution)
+            print(f"max for blueprint {blueprint.b_id} is {best_solution.ores[Geode()]}")
+    return max_geode
 
 
 if __name__ == '__main__':
-    print(main("test_input.txt"))
+    result = main2("input.txt")
+    geodes = 1
+    for i in result:
+        geodes *= i
+    print(geodes)
--- 12-19/model.py
+++ 12-19/model.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+from math import ceil
+
 class Mineral:
     name = ""
     def __init__(self, name):
@@ -5,6 +8,13 @@ class Mineral:
 
     def __str__(self):
         return self.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+
+    def __hash__(self):
+        return self.name.__hash__()
 
 
 class Ore(Mineral):
@@ -22,51 +32,185 @@ class Geode(Mineral):
     def __init__(self):
         super().__init__("Geode")
 
-
 class Robot:
-    produced_mineral = Mineral("")
+    name = ""
+
     def __init__(self, ore: int, clay: int, obsidian: int):
-        self.needed_mineral = dict()
-        self.needed_mineral[Ore] = ore
-        self.needed_mineral[Clay] = clay
-        self.needed_mineral[Obsidian] = obsidian
+        self.needed_minerals = dict()
+        self.needed_minerals[Ore()] = ore
+        self.needed_minerals[Clay()] = clay
+        self.needed_minerals[Obsidian()] = obsidian
 
     def produce(self):
-        return self.produced_mineral
+        return Mineral("")
 
     def __str__(self):
-        return f"{self.produced_mineral} - {self.needed_mineral}"
+        return f"{self.name} - {self.needed_minerals}"
+
+    def __eq__(self, other):
+        return self.name.__eq__(other.name)
+
+    def __hash__(self):
+        return self.name.__hash__()
 
 
 class OreRobot(Robot):
-    produced_mineral = Ore
+    name = "OreRobot"
+
     def __init__(self, ore: int, clay: int, obsidian: int):
         super().__init__(ore, clay, obsidian)
+
+    def produce(self):
+        return Ore()
 
 class ClayRobot(Robot):
-    produced_mineral = Clay
+    name = "ClayRobot"
     def __init__(self, ore: int, clay: int, obsidian: int):
         super().__init__(ore, clay, obsidian)
 
+    def produce(self):
+        return Clay()
+
 class ObsidianRobot(Robot):
-    produced_mineral = Obsidian
+    name = "ObsidianRobot"
     def __init__(self, ore: int, clay: int, obsidian: int):
         super().__init__(ore, clay, obsidian)
+
+    def produce(self):
+        return Obsidian()
+
 class GeodeRobot(Robot):
-    produced_mineral = Geode
+    name = "GeodeRobot"
+    produced_mineral = Geode()
     def __init__(self, ore: int, clay: int, obsidian: int):
         super().__init__(ore, clay, obsidian)
+
+    def produce(self):
+        return Geode()
 
 
 class Blueprint:
+    max_ore_for_robot = 0
+    max_clay_for_robot = 0
+
     def __init__(self, b_id, ore_robot, clay_robot, obsidian_robot, geode_robot):
         self.b_id = b_id
         self.ore_robot = ore_robot
         self.clay_robot = clay_robot
         self.obsidian_robot = obsidian_robot
         self.geode_robot = geode_robot
-
+        self.robots = [self.ore_robot, self.clay_robot, self.obsidian_robot, self.geode_robot]
+        self.max_ore_for_robot = max(max(clay_robot.needed_minerals[Ore()], obsidian_robot.needed_minerals[Ore()]),
+                                     geode_robot.needed_minerals[Ore()])
+        self.max_clay_for_robot = max(int(obsidian_robot.needed_minerals[Clay()]), int(geode_robot.needed_minerals[Clay()]))
 
     def __str__(self):
         return f"{self.b_id} - {self.ore_robot} - {self.clay_robot} - {self.obsidian_robot} - {self.geode_robot}"
 
+
+class Solution:
+    created_at = []
+
+    def __init__(self, blueprint, ores = {Ore() : 0, Clay() : 0, Obsidian() : 0, Geode() : 0}, robots = {}, turn_played = 0, created_at = []):
+        self.blueprint = blueprint
+        self.types_of_robots = blueprint.robots
+        self.turn_played = turn_played
+        self.created_at = created_at
+        if not robots:
+            self.robots = robots
+            self.ores = ores
+            self.create(blueprint.ore_robot, 23)
+            self.ores[Ore()] = 0
+        else:
+            self.ores = ores
+            self.robots = robots
+        self.mineral_mapping = {
+            Ore(): self.blueprint.ore_robot,
+            Clay(): self.blueprint.clay_robot,
+            Obsidian(): self.blueprint.obsidian_robot,
+            Geode(): self.blueprint.geode_robot,
+        }
+
+    def can_create(self, robot):
+        for mineral, number in robot.needed_minerals.items():
+            number = int(number)
+            if number > 0 and (mineral not in self.ores or number > self.ores[mineral]):
+                return False
+        return True
+
+    def time_for_next_robot(self):
+        time_for_robots = {}
+        for robot in self.types_of_robots:
+            if robot in self.robots and robot == self.blueprint.ore_robot and int(self.robots[robot]) >= int(self.blueprint.max_ore_for_robot):
+                continue
+            elif robot in self.robots and robot == self.blueprint.clay_robot and int(self.robots[robot]) >= int(self.blueprint.max_clay_for_robot):
+                continue
+            else:
+                time_for_robots[robot] = self.time_for_robot(robot)
+        return time_for_robots
+
+    def time_for_robot(self, robot):
+        time = 0
+        for mineral, number in robot.needed_minerals.items():
+            number = int(number)
+            if number > 0:
+                if mineral in self.ores:
+                    number = max(0, number - self.ores[mineral])
+                time = max(time, self.time_for_n_mineral(mineral, number))
+        return time
+
+
+    def time_for_n_mineral(self, mineral, number):
+        robot = self.mineral_mapping[mineral]
+        if robot in self.robots and self.robots[robot] > 0:
+            number_of_robots = self.robots[robot]
+            return ceil(number / number_of_robots)
+        else:
+            return 10000
+
+    def create(self, robot, time):
+        self.created_at.append((24 - time, robot.name))
+        for mineral, number in robot.needed_minerals.items():
+            number = int(number)
+            self.ores[mineral] = self.ores[mineral] - number
+        ## if it's a geode, just compute the number of geodes
+        if robot == self.blueprint.geode_robot:
+            if Geode() not in self.ores:
+                self.ores[Geode()] = 0
+            self.ores[Geode()] += time
+        else:
+            if robot not in self.robots:
+                self.robots[robot] = 0
+            self.robots[robot] = self.robots[robot] + 1
+
+
+    def play_turn(self, time):
+        self.turn_played += time
+        for robot, number in self.robots.items():
+            mineral = robot.produce()
+            if mineral in self.ores:
+                self.ores[mineral] = self.ores[mineral] + (number * time)
+            else:
+                self.ores[mineral] = (number * time)
+
+
+    def get_ore(self, mineral):
+        if mineral in self.ores:
+            return self.ores[mineral]
+        else:
+            return 0
+
+    def get_nb_robot(self, robot):
+        if robot in self.robots:
+            return self.robots[robot]
+        else:
+            return 0
+
+
+    def __str__(self):
+        return f"Solution has {self.get_nb_robot(self.blueprint.ore_robot)} ore robot, {self.get_nb_robot(self.blueprint.clay_robot)} clay robot, " \
+               f"{self.get_nb_robot(self.blueprint.obsidian_robot)} obsidian robot and {self.get_nb_robot(self.blueprint.geode_robot)} geode robot" \
+               f". It has {self.get_ore(Ore())} ore, {self.get_ore(Clay())} clay, " \
+               f"{self.get_ore(Obsidian())} obsidian and {self.get_ore(Geode())} geode." \
+               f" Turn_played: {self.turn_played}. " \
+               f"Created at: {self.created_at}"
